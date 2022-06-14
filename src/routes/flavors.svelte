4d2791ca87d38399abd2570b68003cfc57bb07d6
3j5a <3j5a@protonmail.com>
2022-06-15 01:16:08 +0300

    flavors content start

--- src/routes/flavors.svelte
+++ src/routes/flavors.svelte
@@ -1,5 +1,6 @@
 <script>
     import Caret from "$lib/Caret.svelte";
+    import ExternalReference from "$lib/ExternalReference.svelte";
 
     export let codeBlocks;
 </script>
@@ -9,6 +10,12 @@
         <header>
             <h4>flavors</h4>
         </header>
+        <p>
+            Leaving different platforms aside, it's super straightforward with <strong>Julia</strong> - there is only one Julia that runs Julia.
+            So you just go to <ExternalReference href="https://julialang.org/downloads" text="downloads"/>
+            (unless wanna build from <ExternalReference href="https://github.com/JuliaLang/julia" text="source"/> &#129299;)
+            and pick the version.
+        </p>
     </article>
     <Caret/>
 </main>
