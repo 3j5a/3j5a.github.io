06a6162681ebaa1e5b3d5e50861c51eecd53a271
3j5a <3j5a@protonmail.com>
2022-07-10 02:27:12 +0300

    Updates

--- _app/chunks/Tombstone-1659a52a.js
+++
@@ -1,1 +0,0 @@
-import{S as l,i as r,s as i,e as c,c as m,a as h,d as a,b as _,g as d,n as s}from"./index-23842736.js";function v(n){let e;return{c(){e=c("div"),this.h()},l(t){e=m(t,"DIV",{class:!0}),h(e).forEach(a),this.h()},h(){_(e,"class","tombstone svelte-1kbv3o5")},m(t,o){d(t,e,o)},p:s,i:s,o:s,d(t){t&&a(e)}}}class u extends l{constructor(e){super(),r(this,e,null,v,i,{})}}export{u as T};
