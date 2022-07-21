69e662c98cec31be995c003d6233e055c587f44c
3j5a <3j5a@protonmail.com>
2022-07-22 02:18:07 +0300

    Updates

---
+++ _app/pages/why.svelte-5993672a.js
@@ -0,0 +1,63 @@
+import{S as Ge,i as Ke,s as Ne,e as f,t as a,k as Z,x as c,c as m,a as A,h as i,d as l,m as ee,y as d,b as He,g as $,F as r,z as y,r as v,p as _,C as x,n as Fe}from"../chunks/index-4309a87d.js";import{E as B}from"../chunks/ExternalReference-7c074159.js";import{T as Le}from"../chunks/Tombstone-76d80b10.js";import{S as qe}from"../chunks/SideNote-4d873226.js";function Ve(te){let p,b,k,g,R,I,h,t,E,S,C,J,n,w,M,W,H,P,q;return g=new B({props:{href:"https://en.wikipedia.org/wiki/The_Pragmatic_Programmer",text:"The Pragmatic Programmer"}}),h=new B({props:{href:"https://en.wikipedia.org/wiki/The_Matrix",text:"The Matrix"}}),{c(){p=a("1999 was a good year: "),b=f("br"),k=Z(),c(g.$$.fragment),R=a(`,
+                    `),I=f("br"),c(h.$$.fragment),t=a("... "),E=f("br"),S=a(`
+                    Kosovo War, Second Chechen `),C=f("br"),J=a("  War and fcking Putin taking "),n=f("br"),w=a(" over as the acting President."),M=f("br"),W=a(`
+                    I take it back, it wasn't a good `),H=f("br"),P=a("  year at all \u{1F610}")},l(e){p=i(e,"1999 was a good year: "),b=m(e,"BR",{}),k=ee(e),d(g.$$.fragment,e),R=i(e,`,
+                    `),I=m(e,"BR",{}),d(h.$$.fragment,e),t=i(e,"... "),E=m(e,"BR",{}),S=i(e,`
+                    Kosovo War, Second Chechen `),C=m(e,"BR",{}),J=i(e,"  War and fcking Putin taking "),n=m(e,"BR",{}),w=i(e," over as the acting President."),M=m(e,"BR",{}),W=i(e,`
+                    I take it back, it wasn't a good `),H=m(e,"BR",{}),P=i(e,"  year at all \u{1F610}")},m(e,u){$(e,p,u),$(e,b,u),$(e,k,u),y(g,e,u),$(e,R,u),$(e,I,u),y(h,e,u),$(e,t,u),$(e,E,u),$(e,S,u),$(e,C,u),$(e,J,u),$(e,n,u),$(e,w,u),$(e,M,u),$(e,W,u),$(e,H,u),$(e,P,u),q=!0},p:Fe,i(e){q||(v(g.$$.fragment,e),v(h.$$.fragment,e),q=!0)},o(e){_(g.$$.fragment,e),_(h.$$.fragment,e),q=!1},d(e){e&&l(p),e&&l(b),e&&l(k),x(g,e),e&&l(R),e&&l(I),x(h,e),e&&l(t),e&&l(E),e&&l(S),e&&l(C),e&&l(J),e&&l(n),e&&l(w),e&&l(M),e&&l(W),e&&l(H),e&&l(P)}}}function ze(te){let p,b,k,g,R,I,h,t,E,S,C,J;return g=new B({props:{href:"https://en.wikipedia.org/wiki/Computational_science",text:"Computational science"}}),t=new B({props:{href:"https://computationalthinking.mit.edu/Spring21/",text:"Computational thinking"}}),{c(){p=a("Someone said you should be sceptical of anything with \u201Cscience\u201D in its name,"),b=f("br"),k=a(` but
+                    `),c(g.$$.fragment),R=a(` is what aims to solve those complex
+                    problems using computing powers.`),I=f("br"),h=a(`
+                    Don't recall anything similar about "thinking", so safely throwing
+                    `),c(t.$$.fragment),E=a(" in here "),S=f("br"),C=a(" for those of us not in science.")},l(n){p=i(n,"Someone said you should be sceptical of anything with \u201Cscience\u201D in its name,"),b=m(n,"BR",{}),k=i(n,` but
+                    `),d(g.$$.fragment,n),R=i(n,` is what aims to solve those complex
+                    problems using computing powers.`),I=m(n,"BR",{}),h=i(n,`
+                    Don't recall anything similar about "thinking", so safely throwing
+                    `),d(t.$$.fragment,n),E=i(n," in here "),S=m(n,"BR",{}),C=i(n," for those of us not in science.")},m(n,w){$(n,p,w),$(n,b,w),$(n,k,w),y(g,n,w),$(n,R,w),$(n,I,w),$(n,h,w),y(t,n,w),$(n,E,w),$(n,S,w),$(n,C,w),J=!0},p:Fe,i(n){J||(v(g.$$.fragment,n),v(t.$$.fragment,n),J=!0)},o(n){_(g.$$.fragment,n),_(t.$$.fragment,n),J=!1},d(n){n&&l(p),n&&l(b),n&&l(k),x(g,n),n&&l(R),n&&l(I),n&&l(h),x(t,n),n&&l(E),n&&l(S),n&&l(C)}}}function Oe(te){let p,b,k,g,R,I,h,t,E,S,C,J,n,w,M,W,H,P,q,e,u,K,me,N,$e,L,ue,V,ge,pe,he,we,be,ne,ce,de,re,ye,ve,z,_e,O,xe,ke,F,j,Be,ae,Re,Ie,X,Ee,Q,Se,ie,Ce,Je,We,G,je,U,oe;return W=new qe({props:{style:"float: right; margin-top: -5.5em",$$slots:{default:[Ve]},$$scope:{ctx:te}}}),P=new B({props:{href:"https://en.wikipedia.org/wiki/Apache_Groovy",text:"Groovy"}}),e=new B({props:{href:"https://en.wikipedia.org/wiki/Scala_(programming_language)",text:"Scala"}}),K=new B({props:{href:"https://en.wikipedia.org/wiki/Clojure",text:"Clojure"}}),N=new B({props:{href:"https://www.manning.com/books/the-well-grounded-java-developer",text:"Well Grounded Java Develop"}}),L=new B({props:{href:"https://en.wikipedia.org/wiki/Rust_(programming_language)",text:"Rust"}}),V=new B({props:{href:"https://en.wikipedia.org/wiki/Swift_(programming_language)",text:"Swift"}}),z=new B({props:{href:"https://en.wikipedia.org/wiki/Write_once,_run_anywhere",text:"Write once, run anywhere"}}),O=new B({props:{href:"http://brendaneich.github.io/ModernWeb.tw-2015/#74",text:"always bet on JS"}}),X=new B({props:{href:"https://www.youtube.com/watch?v=uLhXgt_gKJc",text:"applications and even microservices"}}),Q=new B({props:{href:"https://julialang.org/packages/",text:"packages"}}),G=new qe({props:{$$slots:{default:[ze]},$$scope:{ctx:te}}}),U=new Le({}),{c(){p=f("main"),b=f("article"),k=f("header"),g=f("h4"),R=a("why"),I=Z(),h=f("section"),t=f("div"),E=a("How about learning a new programming language every year? "),S=f("br"),C=a(`
+                And how about all the frameworks, tools, patterns, architecture
+                styles, best `),J=f("br"),n=a(" practices, devsecdatamlaiuiuxops? \u{1F92F} "),w=f("br"),M=Z(),c(W.$$.fragment),H=a(`
+                A few programming languages appeared since I've started and I've tried (at least to learn)
+                some of them, like
+                `),c(P.$$.fragment),q=a(`,
+                `),c(e.$$.fragment),u=a(`,
+                `),c(K.$$.fragment),me=a(`
+                (talking about `),c(N.$$.fragment),$e=a(` here)
+                , and also `),c(L.$$.fragment),ue=a(`
+                and  `),c(V.$$.fragment),ge=a(`.
+                `),pe=f("br"),he=a(`
+                Must say that all of them are really cool languages and it was worthwhile `),we=f("br"),be=a(` trying them out.
+                But with `),ne=f("b"),ce=a("Java"),de=a(" and "),re=f("b"),ye=a("JavaScript"),ve=a(` already under my belt,
+                I never felt the need to use any other language to get the job done, and
+                I guess more importantly I haven't had a job that needed another language (un or fortunately? \u{1F914}).
+                `),c(z.$$.fragment),_e=a(`
+                and `),c(O.$$.fragment),xe=a("."),ke=Z(),F=f("div"),j=f("p"),Be=a("I never thought of "),ae=f("b"),Re=a("Julia"),Ie=a(` as a general-purpose programming language. While it certainly is and you
+                    can write entire
+                    `),c(X.$$.fragment),Ee=a(` (have they worn out yet?)
+                    in Julia, there are lots of well established languages for that.
+                    For me, the language itself and its ecosystem
+                    (`),c(Q.$$.fragment),Se=a(`)
+                    seems like a perfect tool to explore and work on `),ie=f("b"),Ce=a("actual complex problems"),Je=a(`, by allowing
+                    eloquently expressing and efficiently simulating them.`),We=Z(),c(G.$$.fragment),je=Z(),c(U.$$.fragment),this.h()},l(o){p=m(o,"MAIN",{});var T=A(p);b=m(T,"ARTICLE",{class:!0});var Y=A(b);k=m(Y,"HEADER",{});var le=A(k);g=m(le,"H4",{});var Pe=A(g);R=i(Pe,"why"),Pe.forEach(l),le.forEach(l),I=ee(Y),h=m(Y,"SECTION",{});var se=A(h);t=m(se,"DIV",{});var s=A(t);E=i(s,"How about learning a new programming language every year? "),S=m(s,"BR",{}),C=i(s,`
+                And how about all the frameworks, tools, patterns, architecture
+                styles, best `),J=m(s,"BR",{}),n=i(s," practices, devsecdatamlaiuiuxops? \u{1F92F} "),w=m(s,"BR",{}),M=ee(s),d(W.$$.fragment,s),H=i(s,`
+                A few programming languages appeared since I've started and I've tried (at least to learn)
+                some of them, like
+                `),d(P.$$.fragment,s),q=i(s,`,
+                `),d(e.$$.fragment,s),u=i(s,`,
+                `),d(K.$$.fragment,s),me=i(s,`
+                (talking about `),d(N.$$.fragment,s),$e=i(s,` here)
+                , and also `),d(L.$$.fragment,s),ue=i(s,`
+                and  `),d(V.$$.fragment,s),ge=i(s,`.
+                `),pe=m(s,"BR",{}),he=i(s,`
+                Must say that all of them are really cool languages and it was worthwhile `),we=m(s,"BR",{}),be=i(s,` trying them out.
+                But with `),ne=m(s,"B",{});var Ae=A(ne);ce=i(Ae,"Java"),Ae.forEach(l),de=i(s," and "),re=m(s,"B",{});var Te=A(re);ye=i(Te,"JavaScript"),Te.forEach(l),ve=i(s,` already under my belt,
+                I never felt the need to use any other language to get the job done, and
+                I guess more importantly I haven't had a job that needed another language (un or fortunately? \u{1F914}).
+                `),d(z.$$.fragment,s),_e=i(s,`
+                and `),d(O.$$.fragment,s),xe=i(s,"."),s.forEach(l),ke=ee(se),F=m(se,"DIV",{id:!0});var fe=A(F);j=m(fe,"P",{});var D=A(j);Be=i(D,"I never thought of "),ae=m(D,"B",{});var De=A(ae);Re=i(De,"Julia"),De.forEach(l),Ie=i(D,` as a general-purpose programming language. While it certainly is and you
+                    can write entire
+                    `),d(X.$$.fragment,D),Ee=i(D,` (have they worn out yet?)
+                    in Julia, there are lots of well established languages for that.
+                    For me, the language itself and its ecosystem
+                    (`),d(Q.$$.fragment,D),Se=i(D,`)
+                    seems like a perfect tool to explore and work on `),ie=m(D,"B",{});var Me=A(ie);Ce=i(Me,"actual complex problems"),Me.forEach(l),Je=i(D,`, by allowing
+                    eloquently expressing and efficiently simulating them.`),D.forEach(l),We=ee(fe),d(G.$$.fragment,fe),fe.forEach(l),se.forEach(l),Y.forEach(l),je=ee(T),d(U.$$.fragment,T),T.forEach(l),this.h()},h(){He(F,"id","julia"),He(b,"class","why")},m(o,T){$(o,p,T),r(p,b),r(b,k),r(k,g),r(g,R),r(b,I),r(b,h),r(h,t),r(t,E),r(t,S),r(t,C),r(t,J),r(t,n),r(t,w),r(t,M),y(W,t,null),r(t,H),y(P,t,null),r(t,q),y(e,t,null),r(t,u),y(K,t,null),r(t,me),y(N,t,null),r(t,$e),y(L,t,null),r(t,ue),y(V,t,null),r(t,ge),r(t,pe),r(t,he),r(t,we),r(t,be),r(t,ne),r(ne,ce),r(t,de),r(t,re),r(re,ye),r(t,ve),y(z,t,null),r(t,_e),y(O,t,null),r(t,xe),r(h,ke),r(h,F),r(F,j),r(j,Be),r(j,ae),r(ae,Re),r(j,Ie),y(X,j,null),r(j,Ee),y(Q,j,null),r(j,Se),r(j,ie),r(ie,Ce),r(j,Je),r(F,We),y(G,F,null),r(p,je),y(U,p,null),oe=!0},p(o,[T]){const Y={};T&1&&(Y.$$scope={dirty:T,ctx:o}),W.$set(Y);const le={};T&1&&(le.$$scope={dirty:T,ctx:o}),G.$set(le)},i(o){oe||(v(W.$$.fragment,o),v(P.$$.fragment,o),v(e.$$.fragment,o),v(K.$$.fragment,o),v(N.$$.fragment,o),v(L.$$.fragment,o),v(V.$$.fragment,o),v(z.$$.fragment,o),v(O.$$.fragment,o),v(X.$$.fragment,o),v(Q.$$.fragment,o),v(G.$$.fragment,o),v(U.$$.fragment,o),oe=!0)},o(o){_(W.$$.fragment,o),_(P.$$.fragment,o),_(e.$$.fragment,o),_(K.$$.fragment,o),_(N.$$.fragment,o),_(L.$$.fragment,o),_(V.$$.fragment,o),_(z.$$.fragment,o),_(O.$$.fragment,o),_(X.$$.fragment,o),_(Q.$$.fragment,o),_(G.$$.fragment,o),_(U.$$.fragment,o),oe=!1},d(o){o&&l(p),x(W),x(P),x(e),x(K),x(N),x(L),x(V),x(z),x(O),x(X),x(Q),x(G),x(U)}}}class Ze extends Ge{constructor(p){super(),Ke(this,p,null,Oe,Ne,{})}}export{Ze as default};
