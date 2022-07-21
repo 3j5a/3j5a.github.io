41f2430aaaf64ecd40f14699d7e97136ff91d2ec
3j5a <3j5a@protonmail.com>
2022-07-22 02:17:51 +0300

    re-styling why entry.

--- src/routes/why.svelte
+++ src/routes/why.svelte
@@ -10,36 +10,35 @@
             <h4>why</h4>
         </header>
         <section>
-            <div class="intro">
-                <p>
-                    How about learning a new programming language every year? <br/>
-                    And how about all the frameworks, tools, patterns, architecture
-                    styles, best practices, devsecdatamlaiuiuxops? 🤯 <br/>
-                    A few programming languages appeared since I've started and I've tried (at least to learn)
-                    some of them, like
-                    <ExternalReference href="https://en.wikipedia.org/wiki/Apache_Groovy" text="Groovy"/>,
-                    <ExternalReference href="https://en.wikipedia.org/wiki/Scala_(programming_language)" text="Scala"/>,
-                    <ExternalReference href="https://en.wikipedia.org/wiki/Clojure" text="Clojure"/>
-                    (talking about <ExternalReference href="https://www.manning.com/books/the-well-grounded-java-developer"
-                                                      text="Well Grounded Java Develop"/> here)
-                    , and also <ExternalReference href="https://en.wikipedia.org/wiki/Rust_(programming_language)" text="Rust"/>
-                    and  <ExternalReference href="https://en.wikipedia.org/wiki/Swift_(programming_language)" text="Swift"/>.
-                </p>
-                <SideNote>
-                    1999 was a good year:  <ExternalReference href="https://en.wikipedia.org/wiki/The_Pragmatic_Programmer" text="The Pragmatic Programmer"/>,
-                    <ExternalReference href="https://en.wikipedia.org/wiki/The_Matrix" text="The Matrix"/>... <br/>
-                    Kosovo War, Second Chechen War and fcking Putin taking over as the acting President.<br/>
-                    I take it back, it wasn't a good year at all 😐
+            <div>
+                How about learning a new programming language every year? <br/>
+                And how about all the frameworks, tools, patterns, architecture
+                styles, best <br/> practices, devsecdatamlaiuiuxops? 🤯 <br/>
+                <SideNote style="float: right; margin-top: -5.5em">
+                    1999 was a good year: <br/> <ExternalReference href="https://en.wikipedia.org/wiki/The_Pragmatic_Programmer" text="The Pragmatic Programmer"/>,
+                    <br/><ExternalReference href="https://en.wikipedia.org/wiki/The_Matrix" text="The Matrix"/>... <br/>
+                    Kosovo War, Second Chechen <br/>  War and fcking Putin taking <br/> over as the acting President.<br/>
+                    I take it back, it wasn't a good <br/>  year at all 😐
                 </SideNote>
-            </div>
-            <div class="jjj">
+                A few programming languages appeared since I've started and I've tried (at least to learn)
+                some of them, like
+                <ExternalReference href="https://en.wikipedia.org/wiki/Apache_Groovy" text="Groovy"/>,
+                <ExternalReference href="https://en.wikipedia.org/wiki/Scala_(programming_language)" text="Scala"/>,
+                <ExternalReference href="https://en.wikipedia.org/wiki/Clojure" text="Clojure"/>
+                (talking about <ExternalReference href="https://www.manning.com/books/the-well-grounded-java-developer"
+                                                  text="Well Grounded Java Develop"/> here)
+                , and also <ExternalReference href="https://en.wikipedia.org/wiki/Rust_(programming_language)" text="Rust"/>
+                and  <ExternalReference href="https://en.wikipedia.org/wiki/Swift_(programming_language)" text="Swift"/>.
+                <br/>
                 Must say that all of them are really cool languages and it was worthwhile <br/> trying them out.
                 But with <b>Java</b> and <b>JavaScript</b> already under my belt,
                 I never felt the need to use any other language to get the job done, and
                 I guess more importantly I haven't had a job that needed another language (un or fortunately? 🤔).
                 <ExternalReference href="https://en.wikipedia.org/wiki/Write_once,_run_anywhere" text="Write once, run anywhere"/>
                 and <ExternalReference href="http://brendaneich.github.io/ModernWeb.tw-2015/#74" text="always bet on JS"/>.
-                <p id="julia">
+            </div>
+            <div id="julia">
+                <p>
                     I never thought of <b>Julia</b> as a general-purpose programming language. While it certainly is and you
                     can write entire
                     <ExternalReference href="https://www.youtube.com/watch?v=uLhXgt_gKJc"
@@ -64,14 +63,3 @@
     </article>
     <Tombstone/>
 </main>
-
-<style>
-    .intro {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .jjj {
-        margin-top: -1em;
-    }
-</style>
