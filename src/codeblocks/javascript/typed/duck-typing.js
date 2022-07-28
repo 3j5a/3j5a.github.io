706b9fbf8b07b093bd0b7125b6fa6be95088a188
3j5a <3j5a@protonmail.com>
2022-07-28 03:46:11 +0300

    typed - duck-typing silly addition to JS.

--- src/codeblocks/javascript/typed/duck-typing.js
+++ src/codeblocks/javascript/typed/duck-typing.js
@@ -1,27 +1,29 @@
 let ifItWalksLikeADuck = it => !!it.waddle && it.waddle() === "waddle";
 let itQuacksLikeADuck = it => !!it.quack && it.quack() === "quack";
 let itMustBeADuck = it => ifItWalksLikeADuck(it) && itQuacksLikeADuck(it);
-let lordLoveA = it => console.log(`Lord love a ${it.constructor.name}!`);
+let lordLove = it => console.log(`Lord love ${it}!`);
 
 class Duck {
     waddle() { return "waddle"; };
     quack() { return "quack" };
+    toString() { return "a Duck"; }
 }
 
-class LectricDuck {
+class ElectricDuck {
     mostPeopleGetHappy() {};
     sickInTheMind() {};
     T_H_M() {};
+    toString() { return "The Electric Duck"; }
 }
 
 let duck = new Duck();
 if (itMustBeADuck(duck)) {
-    lordLoveA(duck);
+    lordLove(duck);
 }
-let lectricDuck = new LectricDuck();
-console.log(itMustBeADuck(lectricDuck));
-lectricDuck.waddle = () => "waddle";
-lectricDuck.quack = () => "quack";
-if (itMustBeADuck(lectricDuck)) {
-    lordLoveA(lectricDuck);
+let electricDuck = new ElectricDuck();
+console.log(itMustBeADuck(electricDuck));
+electricDuck.waddle = () => "waddle";
+electricDuck.quack = () => "quack";
+if (itMustBeADuck(electricDuck)) {
+    lordLove(electricDuck);
 }
